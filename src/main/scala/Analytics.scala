7b36e7934108f2831952abae7bc78d4092ce131d
John A. De Goes <john@degoes.net>
2012-05-25 16:16:39 -0600

    code cleanup

--- src/main/scala/Analytics.scala
+++ src/main/scala/Analytics.scala
@@ -1,16 +1,14 @@
 
 object Analytics {
-  import DT.{identity, monoid, rep3, count}
-  
-  val id = identity[Double]
+  import DT.{identity, monoid, rep3, merge3, count}
   
   val sum = monoid[Double](0.0, _ + _)
   
   val product = monoid[Double](1.0, _ * _)
   
-  val square = id.map(v => v * v)
+  val square = identity[Double].map(v => v * v)
   
-  val stdDev = (rep3[Double] >>> (count & sum & square)).map {
-    case ((s0, s1), s2) => math.sqrt((s0 * s2 - s1 * s1) / (s0 * (s0 - 1.0)))
+  val stdDev = (rep3[Double] >>> (count & sum & square)) >>> merge3 { 
+    case (s0, s1, s2) => math.sqrt((s0 * s2 - s1 * s1) / (s0 * (s0 - 1.0)))
   }
 }
